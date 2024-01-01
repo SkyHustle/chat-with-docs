06f98a476ed5fba35c6de95bbd5ada01508915d5
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-01 10:53:41 -0700

    Initialize Shadcn/ui

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,6 @@
-import clsx, { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-    // twMerge will mx-4 my-4 -> m-4
-    // clsx inforces class order
-    return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
