b82f0cef5b703f66af4b64b7a43a3fe2d4a4abec
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-03 20:02:11 -0700

    Turn Navbar into server rendered component and use Clerk helpers to determine user login state

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,9 @@
-"use client";
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
-import { useUser, UserButton, SignInButton } from "@clerk/nextjs";
+import { UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
 
 const Navbar = () => {
-    const { isSignedIn, user, isLoaded } = useUser();
-
     return (
         <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
             <MaxWidthWrapper>
@@ -14,45 +11,39 @@ const Navbar = () => {
                     <Link href="/" className="flex z-40 font-semibold">
                         <span>ChatDoc</span>
                     </Link>
-
-                    {/* <MobileNav isAuth={!!user} /> */}
-
                     <div className="hidden items-center space-x-4 sm:flex">
-                        {!isSignedIn ? (
-                            <>
-                                <Link
-                                    href="/pricing"
-                                    className={buttonVariants({
-                                        variant: "ghost",
-                                        size: "sm",
-                                    })}
-                                >
-                                    Pricing
-                                </Link>
-                                <Link
-                                    href="/auth/sign-in"
-                                    className={buttonVariants({
-                                        variant: "ghost",
-                                        size: "sm",
-                                    })}
-                                >
-                                    Sign In/Up
-                                </Link>
-                            </>
-                        ) : (
-                            <>
-                                <Link
-                                    href="/dashboard"
-                                    className={buttonVariants({
-                                        variant: "ghost",
-                                        size: "sm",
-                                    })}
-                                >
-                                    Dashboard
-                                </Link>
-                                <UserButton afterSignOutUrl="/" />
-                            </>
-                        )}
+                        <SignedOut>
+                            <Link
+                                href="/pricing"
+                                className={buttonVariants({
+                                    variant: "ghost",
+                                    size: "sm",
+                                })}
+                            >
+                                Pricing
+                            </Link>
+                            <Link
+                                href="/auth/sign-in"
+                                className={buttonVariants({
+                                    variant: "ghost",
+                                    size: "sm",
+                                })}
+                            >
+                                Sign In/Up
+                            </Link>
+                        </SignedOut>
+                        <SignedIn>
+                            <Link
+                                href="/dashboard"
+                                className={buttonVariants({
+                                    variant: "ghost",
+                                    size: "sm",
+                                })}
+                            >
+                                Dashboard
+                            </Link>
+                            <UserButton afterSignOutUrl="/" />
+                        </SignedIn>
                     </div>
                 </div>
             </MaxWidthWrapper>
